6c4385f6795eecaeaf923473770407180f55546d
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2022-10-11 14:51:38 -0300

    criando banco externo

--- commons-kafka/src/main/java/ecommerce/KafkaService.java
+++ commons-kafka/src/main/java/ecommerce/KafkaService.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -42,9 +43,8 @@ public class KafkaService<T> implements Closeable {
             for (var record : records) {
                 try {
                     parse.consume(record);
-                }catch (ExecutionException e){
-                    e.printStackTrace();
-                }catch (InterruptedException e){
+                }catch (Exception e){
+                    //catch é exception para pegar qualquer tipo de exceção
                     e.printStackTrace();
                 }
             }
